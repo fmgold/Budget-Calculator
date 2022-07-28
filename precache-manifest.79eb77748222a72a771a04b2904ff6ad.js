e80ca3e7a74a67ba0b03c8cbe2464d6378faaec1
fmgold <fmgold2015@gmail.com>
2022-07-28 05:09:23 +0100

    Updates

---
+++ precache-manifest.79eb77748222a72a771a04b2904ff6ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bce04e4651f0f015e871cf7b71520501",
+    "url": "/Budget-Calculator/index.html"
+  },
+  {
+    "revision": "f72b8abf5c69f60d1800",
+    "url": "/Budget-Calculator/static/css/main.9c88a008.chunk.css"
+  },
+  {
+    "revision": "de104e908fa2aade6326",
+    "url": "/Budget-Calculator/static/js/2.627e4c95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Budget-Calculator/static/js/2.627e4c95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f72b8abf5c69f60d1800",
+    "url": "/Budget-Calculator/static/js/main.3d290418.chunk.js"
+  },
+  {
+    "revision": "fcd4c169a3d3dd9e7ebd",
+    "url": "/Budget-Calculator/static/js/runtime-main.0e20bced.js"
+  }
+]);
